45e811c0b0ad06a1452faa7624b6501819429aca
youngjuning <1003719811@qq.com>
2018-03-09 22:42:21 +0800

    删除与代码块无关的文件

--- spec/atom-leancloud-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import AtomLeancloudView from '../lib/atom-leancloud-view';
-
-describe('AtomLeancloudView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
