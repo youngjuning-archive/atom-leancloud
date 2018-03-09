6c629729313914714ff8413c8cc6ac8b6be6c25b
杨俊宁 <youngjuning@aliyun.com>
2018-03-09 22:37:08 +0800

    Initial commit

---
+++ spec/atom-leancloud-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomLeancloudView from '../lib/atom-leancloud-view';
+
+describe('AtomLeancloudView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
